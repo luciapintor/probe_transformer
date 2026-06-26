7964bf8eb7c025e5d7489bf97fcd161e09c33ffe
Lucia <luciapintor90@gmail.com>
2026-06-26 15:53:13 +0200

    crea cartelle di output

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
+import os
+
 csv_path = "data_dataset/dataset_merged_probes_csv/data_with_label/all_A_full.csv"
 model_path = "data_models/probe_encoder.pt"
 baseline_path = "data_outputs/baseline_clusters.csv"
 embeddings_path = "data_outputs/clusters.csv"
+
+# Crea le cartelle di output se non esistono
+os.makedirs(os.path.dirname(model_path), exist_ok=True)
+os.makedirs(os.path.dirname(baseline_path), exist_ok=True)
+os.makedirs(os.path.dirname(embeddings_path), exist_ok=True)
 
 excluded_features = [
     "label",
